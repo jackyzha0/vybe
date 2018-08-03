9560f89845887e759ee1923c205c4ac17d4d0c6e
unknown <jacky.zhao2019@stgeorges.bc.ca>
2018-08-02 22:02:21 -0700

    Working split channels and 3 second audio queue

--- rec_audio.py
+++ rec_audio.py
@@ -1,5 +1,6 @@
 import pyaudio
 import time
+import Queue
 import numpy as np
 from matplotlib import pyplot as plt
 import scipy.signal as signal
@@ -14,6 +15,8 @@ fulldata = np.array([])
 dry_data = np.array([])
 left = np.array([])
 right = np.array([])
+left_queue = Queue.Queue()
+right_queue = Queue.Queue()
 
 def main():
     stream = p.open(format=pyaudio.paFloat32,
@@ -24,45 +27,48 @@ def main():
         stream_callback=callback)
     stream.start_stream()
     while stream.is_active():
-        time.sleep(2)
-        stream.stop_stream()
-        #pass
+        #21 cycles in a second
+        #Listen for 3 sec
+        #63 cycles
+        #time.sleep(1)
+        #stream.stop_stream()
+        pass
     stream.close()
 
-    numpydata = np.hstack(fulldata)
-    plt.plot(numpydata)
-    plt.title("Wet")
-    plt.show()
-
-
-    numpydata = np.hstack(dry_data)
-    plt.plot(numpydata)
-    plt.title("Dry")
-    plt.show()
-    p.terminate()
-
-    numpydata = np.hstack(left)
-    plt.plot(numpydata)
+    plt.plot(left)
     plt.title("Left")
     plt.show()
     p.terminate()
 
-    numpydata = np.hstack(right)
-    plt.plot(numpydata)
+    plt.plot(right)
     plt.title("Right")
     plt.show()
     p.terminate()
 
 def callback(in_data, frame_count, time_info, flag):
-    global b,a,fulldata,dry_data,frames,left,right
+    global b,a,fulldata,dry_data,frames,left,right,left_queue,right_queue
     audio_data = np.fromstring(in_data, dtype=np.float32)
+    #print('audio_data',audio_data.shape)
     dry_data = np.append(dry_data,audio_data)
     #do processing here
     fulldata = np.append(fulldata,audio_data)
+    #print('fulldata',fulldata.shape)
     result = np.fromstring(audio_data, dtype=np.float32)
-    result = np.reshape(audio_data, (1024, 2))
-    left = np.append(left,result[:, 0])
-    right = np.append(right,result[:, 1])
+    #print('result0',result.shape)
+    #print('result1',result.shape)
+    #print(result[::2] == result[1::2])
+    left = np.append(left,result[::2])
+    left_queue.put(result[::2])
+    right_queue.put(result[::2])
+    while left_queue.qsize() > 63:
+        left_queue.get()
+    while right_queue.qsize() > 63:
+        right_queue.get()
+    print("left_q",left_queue.qsize())
+    print("right_q",right_queue.qsize())
+    #print('left',left.shape)
+    right = np.append(right,result[1::2])
+    #print('right',right.shape)
     return (audio_data, pyaudio.paContinue)
 
 main()
