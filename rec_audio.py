0312494009a640d42bc3eead9424e958e8470973
jackyzha0 <jacky.zhao2019@stgeorges.bc.ca>
2018-08-08 15:41:43 -0400

    Backup

--- rec_audio.py
+++ rec_audio.py
@@ -11,7 +11,7 @@ from preprocess import features
 
 print('import ok')
 
-CHANNELS = 2
+CHANNELS = 4
 RATE = 16000
 
 p = pyaudio.PyAudio()
@@ -19,10 +19,19 @@ fulldata = np.array([])
 dry_data = np.array([])
 left = np.array([])
 right = np.array([])
+top = np.array([])
+bottom = np.array([])
+
 left_feat = np.array([])
 right_feat = np.array([])
+top_feat = np.array([])
+bottom_feat = np.array([])
+
 left_queue = Queue.Queue()
 right_queue = Queue.Queue()
+top_queue = Queue.Queue()
+bottom_queue = Queue.Queue()
+
 feed_dict0 = {}
 feed_dict1 = {}
 feed_dict2 = {}
@@ -63,7 +72,7 @@ def main():
     # imported_meta = tf.train.import_meta_graph("ckpt/model.meta")
     # with tf.Session() as session:
     #     imported_meta.restore(session, tf.train.latest_checkpoint('ckpt'))
-    global left_feat,right_feat,feed_dict0,feed_dict1,feed_dict2,feed_dict3,x_inp,out,sess,graph
+    global left_feat,right_feat,top_feat,bottom_feat,feed_dict0,feed_dict1,feed_dict2,feed_dict3,x_inp,out,sess,graph
     stream = p.open(format=pyaudio.paFloat32,
         channels=CHANNELS,
         rate=RATE,
@@ -84,12 +93,28 @@ def main():
             ftrtmp = np.vstack([ftrtmp,ext_mfccs])
             right_feat = ftrtmp[0]
 
+            mfccs, chroma, mel, contrast,tonnetz = features(np.hstack(list(top_queue.queue)),raw=True)
+            ext_mfccs = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
+            ftrtmp = np.vstack([ftrtmp,ext_mfccs])
+            top_feat = ftrtmp[0]
+
+            mfccs, chroma, mel, contrast,tonnetz = features(np.hstack(list(bottom_queue.queue)),raw=True)
+            ext_mfccs = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
+            ftrtmp = np.vstack([ftrtmp,ext_mfccs])
+            bottom_feat = ftrtmp[0]
+
             feed_dict0 = {x_inp:[left_feat]}
             feed_dict1 = {x_inp:[right_feat]}
+            feed_dict2 = {x_inp:[top_feat]}
+            feed_dict3 = {x_inp:[bottom_feat]}
             left = sess.run((out),feed_dict0)
             right = sess.run((out),feed_dict1)
+            top = sess.run((out),feed_dict2)
+            bottom = sess.run((out),feed_dict3)
             print('Left: '+getHighestProb(left))
             print('Right: '+getHighestProb(right))
+            print('Top: '+getHighestProb(top))
+            print('Bottom: '+getHighestProb(bottom))
         #21 cycles in a second
         #Listen for 3 sec
         #63 cycles
@@ -97,30 +122,9 @@ def main():
         #stream.stop_stream()
         pass
     stream.close()
-    # plt.plot(left)
-    # plt.title("Left")
-    # plt.show()
-    # p.terminate()
-    #
-    # plt.plot(right)
-    # plt.title("Right")
-    # plt.show()
-    # p.terminate()
-
-    plt.plot(left_feat)
-    plt.title("MFCC Left")
-    librosa.display.specshow(left_feat, x_axis='time')
-    plt.show()
-
-    plt.plot(right_feat)
-    plt.title("MFCC Right")
-    librosa.display.specshow(right_feat, x_axis='time')
-    #print(left_feat.shape,right_feat.shape)
-    p.terminate()
-    plt.show()
 
 def callback(in_data, frame_count, time_info, flag):
-    global inp,b,a,fulldata,dry_data,frames,left,right,left_feat,right_feat,left_queue,right_queue,feed_dict0,feed_dict1,feed_dict2,feed_dict3
+    global inp,b,a,fulldata,dry_data,frames,left,right,top,bottom,left_feat,right_feat,top_feat,bottom_feat,left_queue,right_queue,top_queue,bottom_queue,feed_dict0,feed_dict1,feed_dict2,feed_dict3
     audio_data = np.fromstring(in_data, dtype=np.float32)
     #print('audio_data',audio_data.shape)
     dry_data = np.append(dry_data,audio_data)
@@ -128,16 +132,21 @@ def callback(in_data, frame_count, time_info, flag):
     fulldata = np.append(fulldata,audio_data)
     #print('fulldata',fulldata.shape)
     result = np.fromstring(audio_data, dtype=np.float32)
+    result = np.reshape(result, (193, 4))
     #print('result0',result.shape)
     #print('result1',result.shape)
     #print(result[::2] == result[1::2])
-    left_queue.put(result[::2])
-    right_queue.put(result[::2])
+    left_queue.put(result[:,0])
+    right_queue.put(result[:,1])
+    top_queue.put(result[:,2])
+    bottom_queue.put(result[:,3])
     #print(left_queue.qsize())
     while left_queue.qsize() > 63:
         for i in range(21):
             left_queue.get()
             right_queue.get()
+            top_queue.get()
+            bottom_queue.get()
     return (audio_data, pyaudio.paContinue)
 
 main()
